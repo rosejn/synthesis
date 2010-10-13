d72ef7f520954640dc20bdc648026a6dbeb0fbc9
Jeff Rose <rosejn@gmail.com>
2010-10-13 18:00:31 +0200

    getting examples setup

--- src/synth/core.clj
+++ src/synth/core.clj
@@ -1,1 +1,14 @@
-(ns synth.core)
+(ns synth.core
+  (:use overtone.live))
+
+(definst pad [f 440 attack 0.001 release 0.2] 
+         (* (env-gen (perc attack release) :action :free) 
+            (+ (sin-osc (/ f 2)) 
+               (saw [f (* 1.01 f)]))))
+
+(defn play-pad []
+  (doseq [i [440 440 220 330 440]] 
+  (foo i) 
+  (Thread/sleep 250)))
+
+(
